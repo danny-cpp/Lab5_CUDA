20a130cd28e1ec3b840663b1016149af258b66fd
Danh Nguyen <huudanh2908@gmail.com>
2021-03-02 18:55:00 -0500

    add slow kernel

---
+++ Submission/kernel_slow.cu
@@ -0,0 +1,131 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <wb.h>
+
+#define NUM_BINS 4096
+
+#define CUDA_CHECK(ans)                                                   \
+	    { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line,
+	bool abort = true) {
+	if (code != cudaSuccess) {
+		fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code),
+			file, line);
+		if (abort)
+			exit(code);
+	}
+}
+
+__global__ void binning(unsigned int* input_array, unsigned int* bin_array, int input_length, int bin_num) {
+	int i = blockDim.x*blockIdx.x + threadIdx.x;
+
+	//int stride = blockDim.x*gridDim.x;
+
+	if (i < input_length) {
+		unsigned int position = input_array[i];
+		if (position >= 0 && position < NUM_BINS) {
+			if (bin_array[position] < 127)
+				atomicAdd(&bin_array[position], 1);
+			
+		}
+	}
+}
+
+__global__ void limmiting(unsigned int* bin_array, int bin_num) {
+	int i = blockDim.x*blockIdx.x + threadIdx.x;
+
+	if (i < bin_num) {
+		if (bin_array[i] > 127) {
+			bin_array[i] = 127;
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
+	wbArg_t args;
+	int inputLength;
+	unsigned int *hostInput;
+	unsigned int *hostBins;
+	unsigned int *deviceInput;
+	unsigned int *deviceBins;
+
+	args = wbArg_read(argc, argv);
+
+	wbTime_start(Generic, "Importing data and creating memory on host");
+	hostInput = (unsigned int *)wbImport(wbArg_getInputFile(args, 0),
+		&inputLength, "Integer");
+	hostBins = (unsigned int *)malloc(NUM_BINS * sizeof(unsigned int));
+	wbTime_stop(Generic, "Importing data and creating memory on host");
+
+	wbLog(TRACE, "The input length is ", inputLength);
+	wbLog(TRACE, "The number of bins is ", NUM_BINS);
+
+	for (int i = 0; i < NUM_BINS; i++) {
+		hostBins[i] = 0;
+	}
+
+	wbTime_start(GPU, "Allocating GPU memory.");
+	// TODO: Allocate GPU memory here
+	cudaMalloc((void**)&deviceInput, inputLength*sizeof(unsigned int));
+	cudaMalloc((void**)&deviceBins, NUM_BINS*sizeof(unsigned int));
+
+	CUDA_CHECK(cudaDeviceSynchronize());
+	wbTime_stop(GPU, "Allocating GPU memory.");
+
+	wbTime_start(GPU, "Copying input memory to the GPU.");
+	// TODO: Copy memory to the GPU here
+	cudaMemcpy(deviceInput, hostInput, inputLength*sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaMemcpy(deviceBins, hostBins, NUM_BINS*sizeof(unsigned int), cudaMemcpyHostToDevice);
+
+	CUDA_CHECK(cudaDeviceSynchronize());
+	wbTime_stop(GPU, "Copying input memory to the GPU.");
+
+	// Launch kernel
+	// ----------------------------------------------------------
+	wbLog(TRACE, "Launching kernel");
+	wbTime_start(Compute, "Performing CUDA computation");
+
+	// TODO: Perform kernel computation here
+	int thread_num = 256;
+	int block_num = (NUM_BINS + inputLength + thread_num - 1) / thread_num;
+
+	binning<<<block_num, thread_num>>>(deviceInput, deviceBins, inputLength, NUM_BINS);
+
+	limmiting << <block_num, thread_num >> >(deviceBins, NUM_BINS);
+
+
+	// You should call the following lines after you call the kernel.
+	CUDA_CHECK(cudaGetLastError());
+	CUDA_CHECK(cudaDeviceSynchronize());
+
+	wbTime_stop(Compute, "Performing CUDA computation");
+
+	wbTime_start(Copy, "Copying output memory to the CPU");
+	// TODO: Copy the GPU memory back to the CPU here
+
+	cudaMemcpy(hostBins, deviceBins, NUM_BINS*sizeof(unsigned int), cudaMemcpyDeviceToHost);
+
+	CUDA_CHECK(cudaDeviceSynchronize());
+	wbTime_stop(Copy, "Copying output memory to the CPU");
+
+	wbTime_start(GPU, "Freeing GPU Memory");
+	// TODO: Free the GPU memory here
+	cudaFree(deviceBins);
+	cudaFree(deviceInput);
+
+	wbTime_stop(GPU, "Freeing GPU Memory");
+
+	// Verify correctness
+	// -----------------------------------------------------
+	wbSolution(args, hostBins, NUM_BINS);
+
+	free(hostBins);
+	free(hostInput);
+
+#if LAB_DEBUG
+	system("pause");
+#endif
+
+	return 0;
+}
